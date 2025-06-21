43b407b983bfbdc0ac417d8b05fc8867e3c8ef2f
Ikram Akbar <ikramakbar71@gmail.com>
2025-06-22 04:18:52 +0600

    Middlewire : Checking the temp folder exist or not

--- src/middlewires/multer.middlewire.js
+++ src/middlewires/multer.middlewire.js
@@ -1,8 +1,18 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+// Define temp folder safely
+const tempFolder = path.resolve("public/temp");
+
+// Ensure the folder exists
+if (!fs.existsSync(tempFolder)) {
+  fs.mkdirSync(tempFolder, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../public/temp");
+    cb(null,tempFolder);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
